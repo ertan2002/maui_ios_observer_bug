e32098f8fbf570d3f512aa3ab0335a23ed42ceb6
ertan <ertan2002@hotmail.com>
2023-12-06 12:48:32 +0100

    initial comit

---
+++ Ios_Bug/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Ios_Bug
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ Ios_Bug/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿
+namespace Ios_Bug
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();            
+            webview.Navigated += Webview_Navigated;
+            webview.Source = "https://app.trackor.dev/";
+        }
+
+        private void Webview_Navigated(object? sender, WebNavigatedEventArgs e)
+        {
+            Console.WriteLine("Navigated URL"  + e.Url  );
+        }
+
+        protected override void OnHandlerChanged()
+        {
+           // base.OnHandlerChanged();
+#if IOS
+
+            var nativeWebView = webview.Handler.PlatformView as WebKit.WKWebView;
+            if (nativeWebView != null)
+            {
+                nativeWebView.AddObserver(new Foundation.NSString("URL"), Foundation.NSKeyValueObservingOptions.OldNew, (o) =>
+                {
+                    Console.WriteLine("new url is received");
+                });
+            }
+
+#endif
+        }
+    }
+
+}
